e1171e0d0ebf87edaa3806b90f7e97eb62b535d1
kang,hyungmin <hmkkang0922@daum.net>
2023-04-08 19:50:11 +0900

    add test

--- apps/trade-app/src/main.ts
+++ apps/trade-app/src/main.ts
@@ -7,14 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const pathString = join(__dirname, '../../hero/hero.proto');
   console.log(`pathString: ${pathString}`);
-  // app.connectMicroservice<MicroserviceOptions>({
-  //   transport: Transport.GRPC,
-  //   options: {
-  //     url: 'localhost:3002', //`${process.env.ALB_DNS_NAME}:3002`,
-  //     package: 'trade',
-  //     protoPath: join(__dirname, '../../../libs/grpc/proto/trade.proto'),
-  //   },
-  // });
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.GRPC,
+    options: {
+      url: 'localhost:3002', //`${process.env.ALB_DNS_NAME}:3002`,
+      package: 'trade',
+      protoPath: join(__dirname, '../../../libs/grpc/proto/trade.proto'),
+    },
+  });
   await app.startAllMicroservices();
   await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
