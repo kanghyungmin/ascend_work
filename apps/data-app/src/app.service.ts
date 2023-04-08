a9c9664569c861a03444fddfa0c680a44154912b
kang,hyungmin <hmkkang0922@daum.net>
2023-04-08 19:34:46 +0900

    add test

--- apps/data-app/src/app.service.ts
+++ apps/data-app/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class DataAppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello Data APP World!';
   }
 }
--- apps/trade-app/src/main.ts
+++ apps/trade-app/src/main.ts
@@ -7,16 +7,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const pathString = join(__dirname, '../../hero/hero.proto');
   console.log(`pathString: ${pathString}`);
-  app.connectMicroservice<MicroserviceOptions>({
-    transport: Transport.GRPC,
-    options: {
-      url: `${process.env.ALB_DNS_NAME}:3002`,
-      package: 'trade',
-      protoPath: join(__dirname, '../../../libs/grpc/proto/trade.proto'),
-    },
-  });
+  // app.connectMicroservice<MicroserviceOptions>({
+  //   transport: Transport.GRPC,
+  //   options: {
+  //     url: 'localhost:3002', //`${process.env.ALB_DNS_NAME}:3002`,
+  //     package: 'trade',
+  //     protoPath: join(__dirname, '../../../libs/grpc/proto/trade.proto'),
+  //   },
+  // });
   await app.startAllMicroservices();
-  await app.listen(process.env.PORT);
+  await app.listen(3001);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
